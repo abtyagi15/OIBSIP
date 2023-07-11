327f62cf386644cfd864776735325a078936dbde
abtyagi15 <abtyagi150702@gmail.com>
2023-07-11 18:56:32 +0530

    added middleware for the route /abtyagi/v1

--- server/index.js
+++ server/index.js
@@ -3,11 +3,12 @@ const app = express();
 require('dotenv').config();
 const dbConnect = require('./configuration/database');
 const PORT = process.env.PORT || 4000;
+const auth = require('./routes/authRoutes');
 dbConnect();
 
 app.use(express.json());
 
-
+app.use("/abtyagi15/v1",auth);
 app.listen(PORT,()=>{
     console.log("Established server connection at PORT : " + PORT);
 })
