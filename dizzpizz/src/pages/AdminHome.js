cea76b80c79b14060982a7c6a0bdd361a469df21
abtyagi15 <abtyagi150702@gmail.com>
2023-07-12 17:14:28 +0530

    after login and signup these pages will be redirected

---
+++ dizzpizz/src/pages/AdminHome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminHome = () => {
+  return (
+    <div className='text-white'>AdminHome</div>
+  )
+}
+
+export default AdminHome
---
+++ dizzpizz/src/pages/UserHome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserHome = () => {
+  return (
+    <div className='text-white'>UserHome</div>
+  )
+}
+
+export default UserHome
