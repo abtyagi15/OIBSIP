a8c0441e74384fce2db4d47b0c77e94479535137
abtyagi15 <abtyagi150702@gmail.com>
2023-07-12 17:17:11 +0530

    Login signup button appears on /login, /signup and / routes, disappears on other routes

--- dizzpizz/src/components/Navbar.js
+++ dizzpizz/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Logo from "../assests/logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <div className="flex justify-around p-4 bg-[#121618] text-white">
       <Link to="/">
@@ -11,14 +13,31 @@ const Navbar = () => {
           <p className="logo">DizzPizz</p>
         </div>
       </Link>
-      <div>
-        <Link to="/login">
-          <button className="mx-1 border px-2 py-1 rounded-sm">Login</button>
-        </Link>
-        <Link to="/signup">
-          <button className="mx-1 border px-2 py-1 rounded-sm">Signup</button>
-        </Link>
-      </div>
+      {(location.pathname === "/" ||
+        location.pathname === "/login" ||
+        location.pathname === "/signup") 
+        && (
+        <div>
+          <Link to="/login">
+            <button className="mx-1 border px-2 py-1 rounded-sm">Login</button>
+          </Link>
+          <Link to="/signup">
+            <button className="mx-1 border px-2 py-1 rounded-sm">Signup</button>
+          </Link>
+        </div>
+      )}
+      {(location.pathname === "/user" ||
+        location.pathname === "/admin") 
+        && (
+        <div>
+          <Link to="/login">
+            <button className="mx-1 border px-2 py-1 rounded-sm">hello</button>
+          </Link>
+          <Link to="/signup">
+            <button className="mx-1 border px-2 py-1 rounded-sm">world</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
