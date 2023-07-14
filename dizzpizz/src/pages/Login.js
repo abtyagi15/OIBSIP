704468601302f38e453ab131d1a3e6ffb0bf479b
abtyagi15 <abtyagi150702@gmail.com>
2023-07-14 11:50:49 +0530

    login page css done

--- dizzpizz/src/pages/Login.js
+++ dizzpizz/src/pages/Login.js
@@ -5,39 +5,43 @@ const Login = () => {
   const [password, setPassword] = useState("hidden");
 
   return (
-    <div className="flex">
-      <div className="bg-white text-black">
-        <form>
-          <input type="email" name="email" placeholder="Email" />
-          <div>
+    <div className="flex w-full h-[500px] rounded-[50px]">
+      <div className="bg-white text-black rounded-l-[10px] w-[64%] flex flex-col items-center h-full justify-center">
+        <h1 className="text-[2em] font-bold m-[0.67em] text-[#000000d9]">Login to Your Account</h1>
+        <form className="flex flex-col items-center w-full">
+          <input type="email" name="email" placeholder="Email" 
+            className="text-[14px] my-[5px] p-[15px] w-[53%] bg-[#edf5f3] rounded-[10px] required overflow-visible outline-none border-none" />
+          <div className="w-[53%] flex items-center bg-[#edf5f3] rounded-[10px] my-[5px] p-0">
             <input
               type={password === "hidden" ? "password" : "text"}
               name="password"
               placeholder="Password"
+              className="w-full text-[14px]  py-[15px] pl-[15px]  bg-[#edf5f3] outline-none border-none rounded-l-[10px]"
+              required
             />
             {password === "hidden" ? (
               <AiOutlineEye
                 onClick={() =>
                   setPassword("visible")
                 }
-                className="cursor-pointer"
+                className="cursor-pointer mr-[15px]"
               />
             ) : (
               <AiOutlineEyeInvisible
                 onClick={() =>
                   setPassword("hidden")
                 }
-                className="cursor-pointer"
+                className="cursor-pointer mr-[15px]"
               />
             )}
           </div>
           <p>Forgot Password?</p>
-          <button>Sign In</button>
+          <button className="bg-[#fac564] m-[10px] rounded-[20px] text-[20px] font-bold py-[12px] w-[180px] text-center">Sign In</button>
         </form>
       </div>
-      <div className="bg-[#fac564]">
-        <p>New Here?</p>
-        <p>Sign Up!</p>
+      <div className="bg-[#fac564] w-[36%] rounded-r-[10px] flex flex-col items-center justify-center">
+        <h1 className="text-[#fff] text-[2em] font-bold m-[0.67em]">New Here?</h1>
+        <p className="bg-white rounded-[20px] text-[20px] font-bold py-[12px] w-[180px] text-center">Sign Up!</p>
       </div>
     </div>
   );
