04093ed01235cbfd48f37b642161d1e4ebde52f2
abtyagi15 <abtyagi150702@gmail.com>
2023-07-18 01:04:12 +0530

    control for login is added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -28,19 +28,67 @@ exports.signup = async (req, res) => {
       fullName,
       email,
       address,
-      password:hashPassword,
+      password: hashPassword,
       role,
     });
 
     return res.status(200).json({
-        success:true,
-        message:"Entry created successfully",
-        role: role
-    })
+      success: true,
+      message: "Entry created successfully",
+      role: role,
+    });
   } catch (error) {
     res.json({
       success: false,
       message: "Unable to signup, try again later",
     });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //person entering email and password
+    if(!email || !password){
+      return res.json({
+        success:false,
+        message:"Please Enter details carefully"
+      })
+    }
+
+    //account does not exist thing
+    const account = await AuthModel.findOne({email});
+    if(!account){
+      return res.json({
+        success:false,
+        message:"Please!! Signup first, no account exist with this email"
+      })
+    }
+
+    //verification
+    if(await bcrypt.compare(password,account.password)){
+      return res.json({
+        success:true,
+        message: "Successfully loged in",
+        role: account.role
+      })
+
+    }
+    else{
+      return res.status(401).json({
+        success:false,
+        message:"Invalid Password"
+      })
+    }
+
+    
+
+   
+  } catch (error) {
+    res.json({
+      success:false,
+      message: "Unable to login"
+    })
   }
 };
